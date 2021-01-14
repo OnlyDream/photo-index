92e94aa97fe1ff0eee07d415e628bbf9b9ff6cdb
Telmo Martins <telmomartins@MacBook-Pro-de-Telmo.local>
2021-01-14 23:28:15 +0000

    Starting with SCSS structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../assets/scss/main.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            hi
+        </div>
+    );
+};
+
+export default App;
